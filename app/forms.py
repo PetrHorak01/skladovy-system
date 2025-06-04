6b7ccc0d76bf8d7ff6bb193b29d826ac9204a49f
Petr Horak <horak.petr01@seznam.cz>
2025-06-04 19:14:38 +0300

    Přidána InventuraForm do app/forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,21 +1,21 @@
+# app/forms.py
+
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, IntegerField, SelectField
+from wtforms import StringField, PasswordField, SubmitField, IntegerField, SelectField, HiddenField, BooleanField
 from wtforms.validators import DataRequired, NumberRange
-from wtforms import HiddenField, BooleanField
 
 class LoginForm(FlaskForm):
     username    = StringField('Uživatelské jméno', validators=[DataRequired()])
     password    = PasswordField('Heslo', validators=[DataRequired()])
-    remember_me = BooleanField('Pamatovat si mě')  # ← toto přidá
+    remember_me = BooleanField('Pamatovat si mě')
     submit      = SubmitField('Přihlásit')
 
 class AddProductForm(FlaskForm):
     name = StringField('Název produktu', validators=[DataRequired()])
-    category = HiddenField()  # Kategorie se nepřepisuje při editaci
+    category = HiddenField()
     color = StringField('Barva')
     back_solution = StringField('Řešení zad')
     submit = SubmitField('Přidat produkt')
-
 
 class StockForm(FlaskForm):
     product_id = SelectField("Produkt", coerce=int, validators=[DataRequired()])
@@ -29,40 +29,23 @@ class StockForm(FlaskForm):
     ], validators=[DataRequired()])
     submit = SubmitField("Naskladnit")
 
-
 class UserForm(FlaskForm):
-    username = StringField(
-        'Uživatelské jméno',
-        validators=[DataRequired()]
-    )
-    password = PasswordField(
-        'Heslo',
-        validators=[DataRequired()]
-    )
-    role = SelectField(
-        'Role',
-        choices=[('admin', 'Admin'), ('skladnik', 'Skladník')],
-        validators=[DataRequired()]
-    )
-    sklad = SelectField(
-        'Sklad (pro skladníka)',
-        choices=[
-            ('Praha', 'Praha'),
-            ('Brno', 'Brno'),
-            ('Pardubice', 'Pardubice'),
-            ('Ostrava', 'Ostrava')
-        ],
-        validators=[DataRequired()]
-    )
+    username = StringField('Uživatelské jméno', validators=[DataRequired()])
+    password = PasswordField('Heslo', validators=[DataRequired()])
+    role = SelectField('Role', choices=[('admin', 'Admin'), ('skladnik', 'Skladník')], validators=[DataRequired()])
+    sklad = SelectField('Sklad (pro skladníka)', choices=[
+        ('Praha', 'Praha'),
+        ('Brno', 'Brno'),
+        ('Pardubice', 'Pardubice'),
+        ('Ostrava', 'Ostrava')
+    ], validators=[DataRequired()])
     submit = SubmitField('Přidat uživatele')
 
-    
 class SalesForm(FlaskForm):
     user_id = SelectField('Uživatel', coerce=int, validators=[DataRequired()])
     try_count = IntegerField('Počet zkoušek', validators=[DataRequired()])
     success_count = IntegerField('Počet prodejů', validators=[DataRequired()])
     submit = SubmitField('Uložit')
-
 
 class NaskladnitForm(FlaskForm):
     sklad      = SelectField('Sklad', choices=[], validators=[DataRequired()])
@@ -77,3 +60,8 @@ class VyskladnitForm(FlaskForm):
     size       = SelectField('Velikost', coerce=str, choices=[])
     quantity   = IntegerField('Počet', validators=[DataRequired(), NumberRange(min=0)])
     submit     = SubmitField('Vyskladnit')
+
+class InventuraForm(FlaskForm):
+    submit_save    = SubmitField('Uložit inventuru')
+    submit_preview = SubmitField('Náhled')
+    submit_confirm = SubmitField('Potvrdit inventuru')
